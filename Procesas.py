0275f0386dd20a11b585f00bb852fafd0f5a2aaa
Katekizmas <49523194+Katekizmas@users.noreply.github.com>
2019-05-09 23:10:56 +0100

    Add files via upload
    Kodas skirtas PI funkcijoms atlikti, reikia papildyti su duomenu bazes prisijungimu

---
+++ Procesas.py
@@ -0,0 +1,44 @@
+import P3kamera
+#import picamera
+import os
+from datetime import datetime
+import subprocess
+from time import sleep
+
+judesioBukle = False
+nuotraukosKelias = "/home/pi/Documents/Nuotraukos/"
+
+def fotografuoti(esantisLaikas, nuotraukosKelias):
+    #nuotraukosPavadinimas = esantisLaikas.strftime("%Y.%m.%d-%H%M%S") + '.jpg'
+    nuotraukosPavadinimas = "masina2" + '.jpg'
+    with picamera.PiCamera() as kamera:
+        kamera.resolution = (1280, 720)
+        kamera.capture(nuotraukosKelias + nuotraukosPavadinimas)
+        print("nufotkinta")
+        return nuotraukosPavadinimas
+def gautiLaika():
+    esantisLaikas = datetime.now()
+    return esantisLaikas
+
+def gautiNumerius(nuotraukosKelias, nuotraukosPavadinimas):
+    gautiNumeris = subprocess.Popen("alpr -c eu " + nuotraukosKelias + nuotraukosPavadinimas + " | tail | head -1 | awk '{print $2}'", shell=True, stdout=subprocess.PIPE).stdout
+    numeris = gautiNumeris.read().decode().strip()
+    if(numeris == "license"):
+        print("Numeris nuotraukoje neatpazintas")
+        numeris = "neatpazintas"
+    else:
+        print("Nuotraukoje numeris yra : " + numeris)
+    sleep(10)
+    return numeris
+
+while True:
+    #judesioBukle = P3kamera.motion()
+    judesioBukle = True
+    print(judesioBukle)
+    if judesioBukle:
+        esantisLaikas = gautiLaika()
+        #nuotraukosPavadinimas = fotografuoti(esantisLaikas, nuotraukosKelias)
+        nuotraukosPavadinimas = "masina2" + '.jpg'
+        numeris = gautiNumerius(nuotraukosKelias, nuotraukosPavadinimas);
+        
+        
