24c5983264454a912a64384325dab90cd3b266e1
Katekizmas <49523194+Katekizmas@users.noreply.github.com>
2019-05-16 00:32:05 +0300

    Add files via upload

--- Procesas.py
+++ Procesas.py
@@ -8,8 +8,8 @@ from time import sleep
 judesioBukle = False
 nuotraukosKelias = "/home/pi/Documents/Nuotraukos/"
 
-def fotografuoti(esantisLaikas, nuotraukosKelias):
-    #nuotraukosPavadinimas = esantisLaikas + '.jpg'
+def fotografuoti():
+    #nuotraukosPavadinimas = gautiLaika() + '.jpg'
     nuotraukosPavadinimas = "masina2" + '.jpg'
     with picamera.PiCamera() as kamera:
         kamera.resolution = (1280, 720)
@@ -20,7 +20,7 @@ def gautiLaika():
     esantisLaikas = datetime.now()
     return esantisLaikas.strftime("%Y-%m-%d %H:%M:%S")
 
-def gautiNumerius(nuotraukosKelias, nuotraukosPavadinimas):
+def gautiNumerius(nuotraukosPavadinimas):
     gautiNumeris = subprocess.Popen("alpr -c eu " + nuotraukosKelias + nuotraukosPavadinimas + " | tail | head -1 | awk '{print $2}'", shell=True, stdout=subprocess.PIPE).stdout
     numeris = gautiNumeris.read().decode().strip()
     if(numeris == "license"):
@@ -28,18 +28,15 @@ def gautiNumerius(nuotraukosKelias, nuotraukosPavadinimas):
         numeris = "neatpazintas"
     else:
         print("Nuotraukoje numeris yra : " + numeris)
-    sleep(10)
     return numeris
 
 while True:
     #judesioBukle = P3kamera.motion()
     judesioBukle = True
-    print(judesioBukle)
     if judesioBukle:
-        esantisLaikas = gautiLaika()
-        print(esantisLaikas)
-        #nuotraukosPavadinimas = fotografuoti(esantisLaikas, nuotraukosKelias)
+        #nuotraukosPavadinimas = fotografuoti()
         nuotraukosPavadinimas = "masina2" + '.jpg'
-        numeris = gautiNumerius(nuotraukosKelias, nuotraukosPavadinimas);
+        numeris = gautiNumerius(nuotraukosPavadinimas);
+        sleep(5)
         
         
